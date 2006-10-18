cbeb138bb3738c9e236e2fd414ddd64a562f61b2
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-10-18 13:24:34 +0000

    added batch utility cleaned up option files

---
+++ test/batch.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+# Author: C. Bernet 2005
+
+#use strict;
+use diagnostics;
+use Getopt::Long;
+
+my $runmin = -1;
+my $runmax = 999999999;
+my $regexp = "";
+GetOptions ('min=s' => \$runmin,
+	    'max=s' => \$runmax, 
+	    'regexp=s'=> \$regexp );
+
+my $masterfile = shift;
+my $castordir = shift;
+
+my @rootfiles = `nsls $castordir`;
+
+print "input files  :\n";
+
+foreach my $rootfile (@rootfiles) {
+    print "\t\t$rootfile";
+}
+print "master       : $masterfile\n";
+
+
+foreach my $rootfile (@rootfiles) {
+    chomp $rootfile;
+    print "processing : $rootfile\n";
+
+    if($rootfile =~/(\d+).root/) {
+	my $run = $1;
+	if( ($run > $runmax) || ($run<$runmin) ) {
+	    print "skipping $run > $runmax or $run < $runmin  \n";
+	    next;
+	}
+    }
+    
+    if($rootfile !~ /$regexp/) {
+	print "skipping, does not match $regexp\n";
+	next;
+    }
+
+    my $outfile = "auto_$masterfile";
+
+    open(IN, "<$masterfile");
+    open(OUT, ">$outfile");
+    while ( <IN> ) {
+	my $line = $_;
+	    
+	if($line =~ /untracked vstring fileNames/) {
+	    print OUT "untracked vstring fileNames = {'rfio:$castordir/$rootfile'}\n";
+	}
+	elsif($line =~ /untracked string fileName/) {
+	    print OUT "untracked string fileName = \"b_$rootfile\" \n";
+	}
+	else {
+	    print OUT "$line";
+	}
+    }
+    close(IN);
+    close(OUT);
+
+    `cmsRun $outfile`;
+}
+
