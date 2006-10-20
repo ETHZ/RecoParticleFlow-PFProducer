1720f2192a00471696066e5aac12c5ce89060ac2
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-10-20 10:36:17 +0000

    removed obsolete code in PFRootEventManager

--- test/init.C
+++ test/init.C
@@ -1,6 +1,7 @@
 {
 
 gSystem->Load("libFWCoreFWLite.so");
+gSystem->Load("libRecoParticleFlowPFProducer.so");
 AutoLibraryLoader::enable();
 gSystem->Load("libCintex.so");
 ROOT::Cintex::Cintex::Enable();
