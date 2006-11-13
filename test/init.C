6a788200807f72dc84c55c7b4f48cb2f7535272e
cmsbuild <>
2006-11-13 14:57:32 +0000

    This commit was manufactured by cvs2git to create tag 'RecoParticleFlow- PFProducer-testtag'.
    Sprout from master 2006-11-13 14:57:31 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'took into account BS e-'
    Delete:
        BuildFile
        data/pFDefaults.cff
        data/resmap_ECAL_eta.dat
        data/resmap_ECAL_phi.dat
        data/resmap_HCAL_eta.dat
        data/resmap_HCAL_phi.dat
        data/tauHadronDecayFilter.cff
        interface/PFProducer.h
        interface/TauHadronDecayFilter.h
        setup_pflow
        src/PFProducer.cc
        src/SealModule.cc
        test/PhysVal_ParticleFlow_Electrons_0_50_NoTracker.cfg
        test/PhysVal_ParticleFlow_Gammas_0_200_NoTracker.cfg
        test/PhysVal_ParticleFlow_pi0s_0_20_NoTracker.cfg
        test/allReco.cff
        test/allReco.cfg
        test/batch.pl
        test/crab.cfg
        test/filter.cfg
        test/init.C
        test/singlegamma_all.cfg
        test/singlegamma_pf.cfg
        test/singlegamma_producer.cfg
        test/singlegamma_reco.cfg
        test/singlegamma_repf.cfg
        test/singlegamma_source.cff
        test/singlepi_all.cfg
        test/singlepi_pf.cfg
        test/singlepi_producer.cfg
        test/singlepi_reco.cfg
        test/singlepi_repf.cfg
        test/singlepi_source.cff
        test/singletau_all.cfg
        test/singletau_pf.cfg
        test/singletau_producer.cfg
        test/singletau_reco.cfg
        test/singletau_repf.cfg
        test/singletau_source.cff
        test/testPFProducer.cfg

--- test/init.C
+++
@@ -1,9 +0,0 @@
-{
-
-gSystem->Load("libFWCoreFWLite.so");
-gSystem->Load("libRecoParticleFlowPFProducer.so");
-AutoLibraryLoader::enable();
-gSystem->Load("libCintex.so");
-ROOT::Cintex::Cintex::Enable();
-
-}
