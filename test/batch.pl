a970a2698fafc6773e9a8c4a7d8f67a0f1c7613f
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2007-01-22 06:32:00 +0000

    batch.pl now works from/to castor

--- test/batch.pl
+++ test/batch.pl
@@ -4,62 +4,107 @@
 #use strict;
 use diagnostics;
 use Getopt::Long;
-
-my $runmin = -1;
-my $runmax = 999999999;
-my $regexp = "";
-GetOptions ('min=s' => \$runmin,
-	    'max=s' => \$runmax, 
-	    'regexp=s'=> \$regexp );
-
-my $masterfile = shift;
-my $castordir = shift;
+use File::Basename;
 
 
-print "input files  :\n";
+my $sfiles=0;
+my $filePattern=0;
 
-my @rootfiles;
 
-my @tmpfiles = `nsls $castordir`;
+my $masterfile="pfRootEvent.opt";
+
+my $doNotSubmit=0;
+my $help=0;
+
+my $bsub=0;
+
+my $pwd = `pwd`;
+chomp $pwd;
+
+
+my $date = `date +%d%b%Y_%H%M%S`;
+my $scandir = "BatchOut_$date";
+chomp $scandir;
+`mkdir $scandir`;
+`echo "scan.pl @ARGV" > scan.pl.log`;
+`mv scan.pl.log $scandir`;
+
+GetOptions (
+	    'files=s' => \$sfiles,
+	    'pattern=s' => \$filePattern, 
+	    'master=s' => \$masterfile, 
+	    'h' => \$help,
+	    'n' => \$doNotSubmit, 
+	    'b=s' => \$bsub );
+
+if($help) {
+    print "usage : batch.pl -files=\"*.root\" -master myconfig.cfg [-p <regexp>] [-n] [-b \"bsub -q 8nm\"]\n";
+    print " -p <regexp>  : specify a regexp to filter input files\n"; 
+    print " -n           : do not proceed\n"; 
+    print " -b <>        : run on the batch system (LSF)\n";
+    exit(1);
+}
+
+if($doNotSubmit) {
+    print "will do nothing... \n";
+}
+
+print "master : $masterfile\n";
+print "======== $sfiles ======= \n";
+
+
+my $ls = "ls";
+my $basedir = "$sfiles"; # contains directory path
+
+
+if( $sfiles =~ /^\/castor/) {
+#    print "source files are on castor\n";
+    $ls = "nsls";
+    $basedir = "$sfiles";
+}  
+
+my @rootfiles; # will contain base filenames (no path)
+
+my @tmpfiles = `$ls $sfiles`;
 foreach my $file (@tmpfiles) {
     if( $file =~ /\.root$/ ) {
-	push(@rootfiles, $file); 
-	print "\t\t$castordir/$file";
+	if( ( $filePattern && 
+	      $file =~ /$filePattern/ ) ||
+	    !$filePattern ) {
+	    push(@rootfiles, basename($file) );
+		
+	}
     }
 }
 
-print "master       : $masterfile\n";
+
+my $outdir = "$pwd/$scandir";    
+my $outcastordir = "$sfiles/$scandir";    
+`nsmkdir $outcastordir`;
 
 
 foreach my $rootfile (@rootfiles) {
     chomp $rootfile;
-    print "processing : $rootfile\n";
-
-    if($rootfile =~/(\d+).root/) {
-	my $run = $1;
-	if( ($run > $runmax) || ($run<$runmin) ) {
-	    print "skipping $run > $runmax or $run < $runmin  \n";
-	    next;
-	}
-    }
+    my $fullfilename = "$basedir/$rootfile";
+    print "processing :  $fullfilename\n";
     
-    if($rootfile !~ /$regexp/) {
-	print "skipping, does not match $regexp\n";
-	next;
-    }
-
-    my $outfile = "auto_$masterfile";
-
+#    if($svalues == 0) {
+    my $optfile = "$outdir/auto_$rootfile.cfg";
+    my $outrootfile = "$outcastordir/out_$rootfile";
+    
     open(IN, "<$masterfile");
-    open(OUT, ">$outfile");
+    open(OUT, ">$optfile");
     while ( <IN> ) {
 	my $line = $_;
-	    
+	if($line =~ /^\s*\#/) { # commented line
+	    print OUT "$line";	    
+	    next;
+	}
 	if($line =~ /untracked vstring fileNames/) {
-	    print OUT "untracked vstring fileNames = {'rfio:$castordir/$rootfile'}\n";
+	    print OUT "untracked vstring fileNames = {'rfio:$fullfilename'}\n";
 	}
 	elsif($line =~ /untracked string fileName/) {
-	    print OUT "untracked string fileName = \"b_$rootfile\" \n";
+	    print OUT "untracked string fileName = \"rfio:$outrootfile\" \n";
 	}
 	else {
 	    print OUT "$line";
@@ -67,7 +112,21 @@ foreach my $rootfile (@rootfiles) {
     }
     close(IN);
     close(OUT);
+    
 
-    `cmsRun $outfile`;
+    if(! $doNotSubmit) {
+#	    my $outrootfile = "$scandir/out_$rootfile";
+	if( !$bsub ) { # standard execution
+	    `nice -n 5 cmsRun $optfile`;
+#		`mv out.root $outrootfile`;
+	}
+	else { # batch execution 
+	    # print "batch \n";
+	    `$bsub  cmsRun $optfile`;
+	    # `mv out.root $outrootfile`;		
+	}
+	#`cp $macro $scandir`;
+	#`cp $optfile $scandir`;	    
+    }
+    
 }
-
