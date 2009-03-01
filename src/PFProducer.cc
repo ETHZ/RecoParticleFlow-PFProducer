1d703edea470b52553abdd58237cf048b41cf4d6
cmsbuild <>
2009-03-01 18:54:23 +0000

    This commit was manufactured by cvs2git to create tag 'RecoParticleFlow- PFProducer-before_BRANCH226'.
    Sprout from master 2008-09-08 17:03:10 UTC Florian Beaudette <sha1-157dd14e997ea257a213e3ed735c484cb76df422@cern.ch> ' added the download.url file'
    Cherrypick from master 2009-03-01 18:54:19 UTC Patrick Janot <sha1-947b7d2151f410efe7b862ab9e58df9283c85909@cern.ch> 'Make use of nsigma_ECAL to look for photonic energy excess for tracks linked to ECAL clusters only':
        interface/PFProducer.h
        python/particleFlow_cfi.py
        src/PFProducer.cc

--- src/PFProducer.cc
+++ src/PFProducer.cc
@@ -1,9 +1,8 @@
 #include "RecoParticleFlow/PFProducer/interface/PFProducer.h"
 #include "RecoParticleFlow/PFAlgo/interface/PFAlgo.h"
-#include "RecoParticleFlow/PFAlgo/interface/PFAlgoTestBenchElectrons.h"
-#include "RecoParticleFlow/PFAlgo/interface/PFAlgoTestBenchConversions.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "RecoParticleFlow/PFClusterTools/interface/PFEnergyCalibration.h"
+#include "RecoParticleFlow/PFClusterTools/interface/PFClusterCalibration.h"
 
 
 #include <sstream>
@@ -17,17 +16,85 @@ using namespace edm;
 
 
 PFProducer::PFProducer(const edm::ParameterSet& iConfig) {
+  
+  unsigned int newCalib = 
+    iConfig.getParameter<unsigned int>("pf_newCalib");
 
+  // Create a PFClusterCalbration auto_ptr
+  shared_ptr<pftools::PFClusterCalibration> 
+    clusterCalibration( new pftools::PFClusterCalibration() );
+
+  // Initialise function parameters properly.
+  double lowEP0 
+    = iConfig.getParameter<double>("pfcluster_lowEP0");
+  double lowEP1 
+    = iConfig.getParameter<double>("pfcluster_lowEP1");
+  double globalP0 
+    = iConfig.getParameter<double>("pfcluster_globalP0");
+  double globalP1 
+    = iConfig.getParameter<double>("pfcluster_globalP1");
+  // 
+  clusterCalibration->setCorrections(lowEP0, lowEP1, globalP0, globalP1);
  
+  unsigned int allowNegative    
+	= iConfig.getParameter<unsigned int>("pfcluster_allowNegative");
+  clusterCalibration->setAllowNegativeEnergy(allowNegative);
+	
+  unsigned int doCorrection
+    = iConfig.getParameter<unsigned int>("pfcluster_doCorrection");
+  clusterCalibration->setDoCorrection(doCorrection);
+ 
+  double barrelEta
+    = iConfig.getParameter<double>("pfcluster_barrelEndcapEtaDiv");
+  clusterCalibration->setBarrelBoundary(barrelEta);
+	
+  /* Now obsolete 
+  double ecalEcut = 
+    iConfig.getParameter<double>("pfcluster_ecalECut");
+  double hcalEcut = 
+    iConfig.getParameter<double>("pfcluster_hcalECut");
 
+  clusterCalibration->setEcalHcalEnergyCuts(ecalEcut,hcalEcut);
+  */
+ 
+  std::vector<std::string>* names = clusterCalibration->getKnownSectorNames();
+  for(std::vector<std::string>::iterator i = names->begin(); i != names->end(); ++i) {
+    std::string sector = *i;
+    std::vector<double> params
+    = iConfig.getParameter<std::vector<double> >(sector);
+    clusterCalibration->setEvolutionParameters(sector, params);
+  }
+
+  //Finally set eta correction
+  unsigned int doEtaCorrection = iConfig.getParameter<unsigned int>("pfcluster_doEtaCorrection");   
+  clusterCalibration->setDoEtaCorrection(doEtaCorrection);
+
+  std::vector<double> etaCorrectionParams = 
+    iConfig.getParameter<std::vector<double> >("pfcluster_etaCorrection");
+  clusterCalibration->setEtaCorrectionParameters(etaCorrectionParams);
   // use configuration file to setup input/output collection names
+  //std::cout << "Finished initialisaing PFClusterCalibration: it looks like...\n";
+  //std::cout  << *clusterCalibration << std::endl;
+
+  //Done with PFClusterCalibration //
+
   inputTagBlocks_ 
     = iConfig.getParameter<InputTag>("blocks");
+
+
+  usePFElectrons_
+    = iConfig.getParameter<bool>("usePFElectrons");    
+
+  electronOutputCol_
+    = iConfig.getParameter<std::string>("pf_electron_output_col");
+
 
   // register products
   produces<reco::PFCandidateCollection>();
 
-
+  if (usePFElectrons_)
+    produces<reco::PFCandidateCollection>(electronOutputCol_);
+  
   double nSigmaECAL 
     = iConfig.getParameter<double>("pf_nsigma_ECAL");
   double nSigmaHCAL 
@@ -70,13 +137,21 @@ PFProducer::PFProducer(const edm::ParameterSet& iConfig) {
 
   double mvaEleCut
     = iConfig.getParameter<double>("pf_electron_mvaCut");
-  bool usePFElectrons
-    = iConfig.getParameter<bool>("usePFElectrons");  
+
   string mvaWeightFileEleID
     = iConfig.getParameter<string>("pf_electronID_mvaWeightFile");
-  edm::FileInPath path_mvaWeightFileEleID( mvaWeightFileEleID.c_str() );
-  
+
+  string path_mvaWeightFileEleID;
+  if(usePFElectrons_)
+    {
+      path_mvaWeightFileEleID = edm::FileInPath ( mvaWeightFileEleID.c_str() ).fullPath();
+    }
+
   // End PFElectrons Configuration
+
+  bool usePFConversions
+    = iConfig.getParameter<bool>("usePFConversions");  
+  
 
 
   shared_ptr<PFEnergyCalibration> 
@@ -87,11 +162,9 @@ PFProducer::PFProducer(const edm::ParameterSet& iConfig) {
 					  eh_offset,
 					  h_slope,
 					  h_offset,
-					  h_damping ) );
+					  h_damping,
+					  newCalib ) );
 
-  bool   clusterRecovery 
-    = iConfig.getParameter<bool>("pf_clusterRecovery");
-  
   double mvaCut = iConfig.getParameter<double>("pf_mergedPhotons_mvaCut");
   string mvaWeightFile 
     = iConfig.getParameter<string>("pf_mergedPhotons_mvaWeightFile");
@@ -105,23 +178,19 @@ PFProducer::PFProducer(const edm::ParameterSet& iConfig) {
   case 0:
     pfAlgo_.reset( new PFAlgo);
     break;
-  case 1:
-    pfAlgo_.reset( new PFAlgoTestBenchElectrons);
-    break;
-  case 2:
-    pfAlgo_.reset( new PFAlgoTestBenchConversions);
-    break;
-  default:
+   default:
     assert(0);
   }
   
   pfAlgo_->setParameters( nSigmaECAL, 
 			  nSigmaHCAL,
 			  calibration,
-			  clusterRecovery,
+			  clusterCalibration,
+			  newCalib,
 			  PSCut, 
 			  mvaCut, 
 			  path_mvaWeightFile.fullPath().c_str() );
+
   //PFElectrons: call the method setpfeleparameters
   pfAlgo_->setPFEleParameters(chi2EcalGSF,
 			      chi2EcalBrem,
@@ -130,8 +199,11 @@ PFProducer::PFProducer(const edm::ParameterSet& iConfig) {
 			      chi2PsGSF,
 			      chi2PsBrem,
 			      mvaEleCut,
-			      path_mvaWeightFileEleID.fullPath(),
-			      usePFElectrons);
+			      path_mvaWeightFileEleID,
+			      usePFElectrons_);
+
+  pfAlgo_->setPFConversionParameters(usePFConversions);
+
   
   verbose_ = 
     iConfig.getUntrackedParameter<bool>("verbose",false);
@@ -192,6 +264,9 @@ void PFProducer::produce(Event& iEvent,
   auto_ptr< reco::PFCandidateCollection > 
     pOutputCandidateCollection( pfAlgo_->transferCandidates() ); 
   
+
+
+  
   LogDebug("PFProducer")<<"particle flow: putting products in the event"<<endl;
   if ( verbose_ ) std::cout <<"particle flow: putting products in the event. Here the full list"<<endl;
   int nC=0;
@@ -202,5 +277,11 @@ void PFProducer::produce(Event& iEvent,
   }
   
   iEvent.put(pOutputCandidateCollection);
+  if(usePFElectrons_)
+    {
+      auto_ptr< reco::PFCandidateCollection >  
+	pOutputElectronCandidateCollection( pfAlgo_->transferElectronCandidates() ); 
+      iEvent.put(pOutputElectronCandidateCollection,electronOutputCol_);
+    }
 }
 
