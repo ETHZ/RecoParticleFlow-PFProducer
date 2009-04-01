ba42db39f3bbd82a1267b965dd9de4f58cfa74d8
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-04-01 14:07:36 +0000

    deactivated electrons again

--- python/particleFlow_cfi.py
+++ python/particleFlow_cfi.py
@@ -13,7 +13,7 @@ particleFlow = cms.EDProducer("PFProducer",
     debug = cms.untracked.bool(False),
 
     # Use electron identification in PFAlgo
-    usePFElectrons = cms.bool(True),
+    usePFElectrons = cms.bool(False),
     pf_electron_output_col=cms.string('electrons'),
     pf_electronID_mvaWeightFile = cms.string('RecoParticleFlow/PFProducer/data/MVAnalysis_BDT.weights_PfElectrons23Jan.txt'),
                               
